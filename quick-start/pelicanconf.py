76016d8ebc579cab68421e044246dab8b1746398
Owen Peckham <owenpeckham@gmail.com>
2026-07-28 11:16:45 +0100

    Update Pelican-generated site

--- quick-start/pelicanconf.py
+++ quick-start/pelicanconf.py
@@ -19,6 +19,8 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+ARTICLE_EXCLUDES = ['extra']
+
 STATIC_PATHS = ['extra']
 EXTRA_PATH_METADATA = {
     'extra/gd-setup.html': {'path': 'gd-setup.html'},
